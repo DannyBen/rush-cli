a0fc0da6123ce68703a14fbc0171c6cbae8e5e3f
Danny Ben Shitrit <db@dannyben.com>
2019-11-22 20:41:40 +0000

    add rush info

---
+++ src/info_command.sh
@@ -0,0 +1,15 @@
+# Collect variables
+repo=${args[repo]:-default}
+package=${args[package]}
+repo_path=$(config_get "$repo")
+package_path=$repo_path/$package
+infofile=$package_path/info
+
+# Verify we have everything we need
+[[ $repo_path ]] || abort "repo not found: $repo"
+[[ -d $package_path ]] || abort "package not found: $repo/$package"
+[[ -f $infofile ]] || abort "infofile not found: $infofile"
+
+# Show the info file
+green "$repo:$package"
+cat "$infofile"
