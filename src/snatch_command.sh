1690dfb10b1a24a1832fd363b74d1768f09f6fc6
Danny Ben Shitrit <db@dannyben.com>
2020-03-06 10:48:45 +0000

    ensure snatch command cleans up on error

--- src/snatch_command.sh
+++ src/snatch_command.sh
@@ -3,8 +3,13 @@ repo_id=${args[github_user]}
 package=${args[package]}
 path="$HOME/rush-repos/snatched"
 
+cleanup() {
+  rush remove snatched --purge
+}
+
 say "snatch" "$repo_id $package"
+
+trap cleanup EXIT ERR INT TERM
 
 rush clone "$repo_id" "$path" --name snatched
 rush get "snatched:$package"
-rush remove snatched --purge
