c44318a370d82adeaa3f267ff6b316e499ddf374
Danny Ben Shitrit <db@dannyben.com>
2020-03-06 07:39:53 +0000

    improve snatch

--- src/remove_command.sh
+++ src/remove_command.sh
@@ -1,3 +1,11 @@
 repo=${args[repo]}
-config_del "$repo"
+purge=${args[--purge]}
+
 echo "remove $(green "$repo")"
+
+if [[ $purge ]] ; then
+  repo_path=$(config_get "$repo")
+  [[ -d $repo_path ]] && rm -rf $repo_path
+fi
+
+config_del "$repo"
