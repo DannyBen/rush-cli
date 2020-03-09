f1779d06966224f035dbff80ee1c7f80d36cb993
Danny Ben Shitrit <db@dannyben.com>
2020-03-09 20:32:53 +0000

    updates

--- src/clone_command.sh
+++ src/clone_command.sh
@@ -3,6 +3,7 @@ path=${args[path]}
 repo_id=${args[github_user]}
 use_ssh=${args[--ssh]}
 full=${args[--full]}
+ignore=${args[--ignore]}
 default_repo_name=${repo_id%%/*}
 repo_name=${args[--name]:-$default_repo_name}
 
@@ -10,7 +11,7 @@ repo_name=${args[--name]:-$default_repo_name}
 [[ $repo_id = */* ]] || repo_id="$repo_id/rush-repo"
 
 # Set clone URL - ssh or https?
-if [[ $use_ssh ]]; then
+if [[ $use_ssh ]] ; then
   repo_url=git@github.com:$repo_id.git
 else
   repo_url=https://github.com/$repo_id.git
@@ -20,17 +21,38 @@ fi
 [[ $path ]] || path="$HOME/rush-repos/$repo_id"
 
 # Abort if target directory exists
-[[ -d $path ]] && abort "Directory $path already exists."
-
-set -e
-
-# Clone
-say "clone" "$repo_url"
-if [[ $full ]]; then
-  git clone "$repo_url" "$path"
-else
-  git clone --depth 1 "$repo_url" "$path"
+if [[ -d $path ]] ; then
+  if [[ $ignore ]] ; then
+    skip=1
+  else
+    abort "Directory $path already exists."
+  fi
 fi
 
-# Save config
-config_set "$repo_name" "$path"
+# Abort if a repository with this name already exists
+if config_has_key "$repo_name" ; then
+  if [[ $ignore ]] ; then
+    skip=1
+  else
+    abort "The repository is already registered:\n$repo_name = $(config_get "$repo_name")."
+  fi
+fi
+
+if [[ $skip ]] ; then
+  say "clone" "skipping $repo_name (exists)"
+
+else
+  set -e
+
+  # Clone
+  say "clone" "$repo_url"
+  if [[ $full ]]; then
+    git clone "$repo_url" "$path"
+  else
+    git clone --depth 1 "$repo_url" "$path"
+  fi
+
+  # Save config
+  config_set "$repo_name" "$path"
+
+fi
